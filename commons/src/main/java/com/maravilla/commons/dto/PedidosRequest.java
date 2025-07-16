74957c553eae14cb8ae3d6699d72d4c58ad08a82
Luis <l.a.mancera21160@gmail.com>
2025-07-16 06:32:34 -0600

    Se agregan packages, y conexión a BD del microservicio de pedidos

--- commons/src/main/java/com/maravilla/commons/dto/PedidosRequest.java
+++ commons/src/main/java/com/maravilla/commons/dto/PedidosRequest.java
@@ -12,10 +12,22 @@ import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 
 public record PedidosRequest(
-		@NotNull(message = "El total no puede ser null") @Min(value = 1, message = "El total debe ser mayor a uno") Double total,
-		@NotNull(message = "El id del cliente es requerido") Long idCliente,
-		@NotNull(message = "La fecha de creacion no puede ser nula") @PastOrPresent(message = "La fecha de creacion debe ser en pasado o presente") Date fechaCreacion,
-		@NotBlank(message = "el estado es requerido") String estado,
-		@NotNull(message = "La lsta de productos no puede estar vacia") @Size(min = 1, message = "Debe hacer al menos un producto en la lista") List<ProductoPedido> productos) {
+		@NotNull(message = "El total no puede ser null") 
+		@Min(value = 1, message = "El total debe ser mayor a uno") 
+		Double total,
+		
+		@NotNull(message = "El id del cliente es requerido") 
+		Long idCliente,
+		
+		@NotNull(message = "La fecha de creacion no puede ser nula") 
+		@PastOrPresent(message = "La fecha de creacion debe ser en pasado o presente") 
+		Date fechaCreacion,
+		
+		@NotBlank(message = "el estado es requerido") 
+		String estado,
+		
+		@NotNull(message = "La lsta de productos no puede estar vacia") 
+		@Size(min = 1, message = "Debe hacer al menos un producto en la lista") 
+		List<ProductoPedido> productos) {
 
 }
