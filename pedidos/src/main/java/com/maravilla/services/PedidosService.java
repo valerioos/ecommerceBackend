beb5fc5cc08204d4616c92570efcb8dc29ebe59d
anayeli <anayelivaleriovelez0@gmail.com>
2025-07-17 09:22:14 -0600

    actualizacion

--- pedidos/src/main/java/com/maravilla/services/PedidosService.java
+++ pedidos/src/main/java/com/maravilla/services/PedidosService.java
@@ -1,9 +1,12 @@
 package com.maravilla.services;
 
+import org.springframework.stereotype.Service;
+
 import com.maravilla.commons.dto.PedidosRequest;
 import com.maravilla.commons.dto.PedidosResponse;
 import com.maravilla.commons.services.CommonService;
 
+@Service
 public interface PedidosService extends CommonService<PedidosRequest, PedidosResponse>{
 
 	boolean existeProducto(Long id);
